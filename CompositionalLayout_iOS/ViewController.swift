cace0f19df026ca21c3f2634a573cd5a702905ce
HongzCloud <jihong21511@gmail.com>
2023-04-09 21:57:23 +0900

    Compositional Layout 적용

--- CompositionalLayout_iOS/ViewController.swift
+++ CompositionalLayout_iOS/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
         
         collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCell")
         collectionView.dataSource = self
+        collectionView.collectionViewLayout = createBasicListLayout()
     }
 
 
@@ -34,5 +35,31 @@ extension ViewController: UICollectionViewDataSource {
         cell.tagLabel.text = items[indexPath.row]
         
         return cell
+    }
+}
+
+extension ViewController {
+    
+    func createBasicListLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .estimated(80),
+                                             heightDimension: .absolute(48.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+      
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .absolute(48.0))
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                         subitems: [item])
+        // 아이템 간격 설정
+        group.interItemSpacing = .flexible(10)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        // 그룹 간격 설정
+        section.interGroupSpacing = 10
+        // 섹션 인셋 설정
+        section.contentInsets = NSDirectionalEdgeInsets(top: 4, leading: 4, bottom: 2, trailing: 2)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
     }
 }
